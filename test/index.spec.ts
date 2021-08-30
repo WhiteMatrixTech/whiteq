1b6cc26f34454a31519fbb61bc2f2f5088dc01fb
Willin V0 <willin@willin.org>
2021-08-30 16:46:27 +0800

    test: :white_check_mark: update test cases

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,13 +1,24 @@
 import { WhiteQ } from '../src';
 
+let wq: WhiteQ;
+
 describe('Unit Tests', () => {
-  it('class WhiteQ', async () => {
-    const wq = new WhiteQ({
+  beforeAll(() => {
+    wq = new WhiteQ({
       connection: {
         host: process.env.REDIS_HOST || 'localhost'
       }
     });
     expect(wq).toBeDefined();
+  });
+
+  it('addJob', async () => {
+    const job = await wq.addJob('queue', 'job', { str: 'any' }, { lifo: true });
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    expect(job.data?.str).toEqual('any');
+  });
+
+  afterAll(async () => {
     await wq.disconnect();
   });
 });
