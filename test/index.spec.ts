1a243d28203b3d97681e5932f7291b83022e8eb3
Willin V0 <willin@willin.org>
2021-08-30 16:37:22 +0800

    ci: :green_heart: fix redis host

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -2,7 +2,11 @@ import { WhiteQ } from '../src';
 
 describe('Unit Tests', () => {
   it('class WhiteQ', async () => {
-    const wq = new WhiteQ();
+    const wq = new WhiteQ({
+      connection: {
+        host: process.env.REDIS_HOST || 'localhost'
+      }
+    });
     expect(wq).toBeDefined();
     await wq.disconnect();
   });
